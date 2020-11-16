d0d03ffcf06e1eefabc256be4205f8f08305ee35
geunpyochoi <ckp220@kookmin.ac.kr>
2020-11-16 21:43:54 +0900

    주석 추가(근표)

--- Code/CookingKingCooKing_Ver2/app/src/main/java/com/techtown/cookingkingcooking_ver2/user_Recipe.java
+++ Code/CookingKingCooKing_Ver2/app/src/main/java/com/techtown/cookingkingcooking_ver2/user_Recipe.java
@@ -16,15 +16,17 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import org.w3c.dom.Text;
 
+// user_Recipe.java : 회원들이 직접 레시피를 게시하면 등록되는 화면을 나타내는 기능
+
 public class user_Recipe extends AppCompatActivity {
 
-    FirebasePost fb;
-    ImageView userImage;
-    TextView userTitle;
-    TextView userWriter;
-    TextView userMaterial;
-    TextView userRecipe;
-    Bitmap bitmap;
+    FirebasePost fb; //MainActivity에서 받아오는 FirebasePost를 담을 객체
+    ImageView userImage; // 게시한 레시피의 음식 사진
+    TextView userTitle; // 게시한 레시피의 요리 제목
+    TextView userWriter; // 게시한 레시피의 작성자
+    TextView userMaterial; // 게시한 레시피의 요리 재료
+    TextView userRecipe; // 게시한 레시피의 요리 과정
+    Bitmap bitmap; // 이미지를 String에서 bitmap으로 변환하는 과정에서 담을 bitmap
 
 
     @Override
@@ -38,29 +40,34 @@ public class user_Recipe extends AppCompatActivity {
         userMaterial = (TextView) findViewById(R.id.textView5);
         userRecipe = (TextView) findViewById(R.id.textView6);
 
-        Intent intent = getIntent();
-        showshow(intent);
+        Intent intent = getIntent(); // MainActivity에서의 전달을 받는 intent
+        show(intent);
 
         userImage.setImageBitmap(bitmap);
-        userImage.getLayoutParams().width=750;
-        userImage.getLayoutParams().height=750;
-        userTitle.setText(fb.title);
-        userWriter.setText(fb.writer);
-        userMaterial.setText(fb.material);
-        userRecipe.setText(fb.recipe);
+        userImage.getLayoutParams().width=750;// 이미지 크기 조정
+        userImage.getLayoutParams().height=750;// 이미지 크기 조정
+        userTitle.setText(fb.title); // 요리 제목 설정해줌
+        userWriter.setText(fb.writer); // 작성자 값 설정
+        userMaterial.setText(fb.material); // 요리재료 설정
+        userRecipe.setText(fb.recipe); // 요리과정 설정
     }
-    public static Bitmap StringToBitmap(String encodedString) {
+
+    //Method:StringToBitmap()
+    //Function:String형태의 이미지값을 Bitmap 값으로 전환해주는 함수
+   public static Bitmap StringToBitmap(String encodedString) {
         try {
-            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT);
-            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            byte[] encodeByte = Base64.decode(encodedString, Base64.DEFAULT); //Base64를 통해서 String을 decoding하여 배열에 저장
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length); //byte형태로 저장된 이미지를 Bitmap으로 변환
             return bitmap;
-        } catch (Exception e) {
+        } catch (Exception e) { // 예외처리
             e.getMessage();
             return null;
         }
     }
 
-    private void showshow(Intent intent)
+    //Method:show()
+    //Function:fb객체를 MainActivity에서 받은 FirebasePose객체로 초기화하고 String형태의 이미지를 bitmap형으로 변환시켜줌
+    private void show(Intent intent)
     {
         fb = (FirebasePost) intent.getParcelableExtra("firebasePost");
         bitmap = StringToBitmap(fb.image_Bitmap);
